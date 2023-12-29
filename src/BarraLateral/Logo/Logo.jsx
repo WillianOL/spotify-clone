d1b5e5a3ccd721fe167279e6787a4699282eb6c4
Willian Oliveira <williancontato67@gmail.com>
2023-12-29 08:49:39 -0300

    finalização da importação de imagem

--- src/BarraLateral/Logo/Logo.jsx
+++ src/BarraLateral/Logo/Logo.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import "./Logo.scss"
+import logo from "./img/spotifyLogo.svg"
 
 const Logo = () => {
   return (
     <div className='conteinerLogo'>
-      <img src="../../../public/img/spotifyLogo.svg" alt="Spotify Logo" />
+      <img src={logo} alt="Spotify Logo" />
     </div>
   )
 }
